ed3d836f568dbc7eeccd7e87a7b7dfb1baa07ad6
Huraira2717 <122014990+Huraira2717@users.noreply.github.com>
2023-02-09 18:04:29 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var a =  15;
+///////////////////////Question 01//////////
+// var value2 = ++a + ++a - --a + a-- + a - a - a++ - --a;
+// a = 16 + 17 - 16 + 16 -15 + 15 -15 -15
+///////////////////////Question 02//////////
+// var value1 = a++ + ++a  -  --a;
+// a = 15 + 17 - 16
+// console.log(value1)
+// document.write(a)
